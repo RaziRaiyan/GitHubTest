24afda8793810ce627d0199742d1b81beffa8303
Raiyan Razi <raiyanrazi3357@gmail.com>
2019-05-10 12:38:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/raiyan/popstick/ChoseSellerCustomer.java
@@ -0,0 +1,40 @@
+package com.raiyan.popstick;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class ChoseSellerCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_seller_consumer);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
