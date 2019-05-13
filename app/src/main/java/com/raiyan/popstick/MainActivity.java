480d83b30eececeb5061d4a6cdb159933dbaad47
Raiyan Razi <raiyanrazi3357@gmail.com>
2019-05-13 11:48:37 +0530

    [ADD] test method1 added

--- app/src/main/java/com/raiyan/popstick/MainActivity.java
+++ app/src/main/java/com/raiyan/popstick/MainActivity.java
@@ -2,6 +2,7 @@ package com.raiyan.popstick;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void method1(){
+        Toast.makeText(this,"This is raiyan's method1",Toast.LENGTH_SHORT).show();
     }
 }
