1476649943c667b8f97d3efc03ddf60455ab676e
Raiyan Razi <raiyanrazi3357@gmail.com>
2019-05-13 11:52:11 +0530

    [ADD] Toast conflict added

--- app/src/main/java/com/raiyan/popstick/MainActivity.java
+++ app/src/main/java/com/raiyan/popstick/MainActivity.java
@@ -10,9 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"This is toast for checking conflict",Toast.LENGTH_SHORT).show();
     }
 
     public void method1(){
         Toast.makeText(this,"This is raiyan's method1",Toast.LENGTH_SHORT).show();
     }
+
+
 }
